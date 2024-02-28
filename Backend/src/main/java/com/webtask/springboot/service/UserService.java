718827f4178a69e29b5f8b9930c6787aaeac3522
Syahrul Huzaimi <syahrulhuzaimi@gmail.com>
2024-02-28 15:48:47 +0100

    Frontend: Refactored unused code

--- Backend/src/main/java/com/webtask/springboot/service/UserService.java
+++ Backend/src/main/java/com/webtask/springboot/service/UserService.java
@@ -15,30 +15,11 @@ public class UserService {
 
     @Autowired
     UserRepository userRepository;
-    private static final String EXISTING_USERNAME = "username1";
-    private static final String ANOTHER_USERNAME = "username2";
 
     public Optional<User> findUserByUsername(String username){
         Optional<User> user = userRepository.findByUsername(username);
         if(user.isEmpty()) return Optional.empty();
         return user;
-//        return user;
-//        if(EXISTING_USERNAME.equalsIgnoreCase(username)) {
-//            var user = new User();
-//            user.setId(2L);
-//            user.setUsername("username1");
-//            user.setPassword("$2a$12$375lgkCT3GiLuQXelOqIw.a/cIR4dPWr3N7n5CzWBTkVgl1C8vQIi");//test
-//            user.setRoles("ROLE_ADMIN");
-//            return Optional.of(user);
-//        } else if(ANOTHER_USERNAME.equalsIgnoreCase(username)){
-//            var user = new User();
-//            user.setId(3L);
-//            user.setUsername("username2");
-//            user.setPassword("$2a$12$375lgkCT3GiLuQXelOqIw.a/cIR4dPWr3N7n5CzWBTkVgl1C8vQIi");//test
-//            user.setRoles("ROLE_USER");
-//            return Optional.of(user);
-//        }
-//        return Optional.empty();
     }
 
     public void saveUser(RegistrationDto registrationDto){
