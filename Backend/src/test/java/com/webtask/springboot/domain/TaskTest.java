d0adf5633e15107b2aec9458d0aaa6a4a54e376e
Syahrul Huzaimi <syahrulhuzaimi@gmail.com>
2024-02-28 13:32:07 +0100

    Backend: Added empty testclass

---
+++ Backend/src/test/java/com/webtask/springboot/domain/TaskTest.java
@@ -0,0 +1,28 @@
+package com.webtask.springboot.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskTest {
+
+    @Test
+    void getTask() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setTask() {
+    }
+
+    @Test
+    void setUser() {
+    }
+}
