c04c34c83c0a50f48f2ea1ad84195ac22db2f53a
Bin N Huzaimi <sbi@iks-gmbh.com>
2024-05-03 12:15:52 +0200

    Implemented log out fuction on password change

--- Frontend/src/app/dialog-logout/dialog-logout.component.ts
+++ Frontend/src/app/dialog-logout/dialog-logout.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-dialog-logout',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule, RouterModule, MatButtonModule],
   templateUrl: './dialog-logout.component.html',
   styleUrl: './dialog-logout.component.css'
 })
 export class DialogLogoutComponent {
 
+  constructor(
+    private _authService: AuthService,
+    private router: Router,
+  ) {}
+  
+  logout(){
+    this._authService.setLogin(false);
+    this._authService.setAdmin(false);
+    localStorage.clear();
+    this.router.navigateByUrl('/');
+  }
 }
