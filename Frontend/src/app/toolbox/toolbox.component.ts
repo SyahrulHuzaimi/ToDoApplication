d117d756b387b72b68834e722478180002ef6475
Bin N Huzaimi <sbi@iks-gmbh.com>
2024-05-03 15:12:28 +0200

    Menu items in toolbox are properly vertical

--- Frontend/src/app/toolbox/toolbox.component.ts
+++ Frontend/src/app/toolbox/toolbox.component.ts
@@ -3,11 +3,14 @@ import { Router, RouterLink } from '@angular/router';
 import { MaterialModule } from '../material/material.module';
 import { AuthService } from '../services/auth.service';
 import { Subscription } from 'rxjs';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-toolbox',
   standalone: true,
-  imports: [RouterLink, MaterialModule],
+  imports: [RouterLink, MatMenuModule, MatButtonModule, MatToolbarModule],
   templateUrl: './toolbox.component.html',
   styleUrl: './toolbox.component.css'
 })
